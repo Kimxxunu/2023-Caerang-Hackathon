bf778b132c60908d7553c47fbeb88c4076d67c5b
새ë¡ãurimPC <jiurim0316@naver.com>
2023-07-29 22:04:23 +0900

    초기세팅

---
+++ src/main/java/com/teamtag/tagweb/TagwebApplication.java
@@ -0,0 +1,13 @@
+package com.teamtag.tagweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamtag/tagweb/TagwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamtag.tagweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
